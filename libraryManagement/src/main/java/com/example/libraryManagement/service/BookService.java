ca8b4fc5d99ee9eb9d5a59c39239bb1a144e1026
benjamin kakai <benjaminkakai001@gmail.com>
2023-07-31 11:38:12 +0300

    Add files via upload

---
+++ libraryManagement/src/main/java/com/example/libraryManagement/service/BookService.java
@@ -0,0 +1,41 @@
+package com.example.libraryManagement.service;
+
+import com.example.libraryManagement.model.Book;
+import com.example.libraryManagement.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if(book != null) {
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
