34914c2dd6f451ba35343474aa35c71e16666568
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 10:44:52 +0300

    Add files via upload

---
+++ LimittheNumberofDigits.java
@@ -0,0 +1,12 @@
+/* public class LimittheNumberofDigits {
+
+    int sum = 0;
+    int limit = 3; // Limit to last 3 digits for example
+while (n > 0 && limit > 0) {
+        sum += n % 10;
+        n /= 10;
+        limit--;
+    }
+
+}
+*/
