a137d54f149511295de4f197b2151d9507047eae
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 10:42:56 +0300

    Add files via upload

---
+++ Constantplusdigit.java
@@ -0,0 +1,14 @@
+/*import static sun.management.counter.Variability.CONSTANT;
+
+public class Constantplusdigit {
+    private static int (int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = (n % 10) + CONSTANT; // Add a constant to each digit
+            sum += digit;
+            n /= 10;
+        }
+
+    }
+}
+*/
