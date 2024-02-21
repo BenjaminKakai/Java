1ea42d720a2bc886922254c3330e9da542d7b139
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 10:41:52 +0300

    Add files via upload

---
+++ AddingandSubtracting.java
@@ -0,0 +1,11 @@
+/*public class AddingandSubtracting {
+    int sum = 0;
+    boolean add = true;
+while (n > 0) {
+        int digit = n % 10;
+        sum += add ? digit : -digit;
+        add = !add; // Toggle between adding and subtracting
+        n /= 10;
+    }
+
+} */
