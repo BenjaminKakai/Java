44291badf22070df516263bed7f6a8741dc158b5
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 11:24:21 +0300

    Add files via upload

--- Superss.java
+++ Superss.java
@@ -1,32 +1,36 @@
 public class Superss {
-    private static int superDigit(int n) {
+    private static int superDigit (int n) {
         if (n < 10) {
             return n;
         }
+
         int sum = 0;
+
         while (n > 0) {
+
             sum += n % 10;
             n /= 10;
         }
         return superDigit(sum);
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         if (args.length < 2) {
-            System.out.println("Please provide n and k as command-line arguments.");
+            System.out.println ("Please input variables n and k as arguments to calculate the superDigit");
             return;
         }
+
         try {
             int n = Integer.parseInt(args[0]);
             int k = Integer.parseInt(args[1]);
 
-            // Initially calculate the super digit of n
-            int initialSuperDigit = superDigit(n);
-            // Then, calculate the super digit of the product of the initial super digit and k
-            int result = superDigit(initialSuperDigit * k);
-            System.out.println("Super digit: " + result);
-        } catch (NumberFormatException e) {
-            System.out.println("Both n and k must be integers.");
+            int i = superDigit(n);
+            int results  = superDigit(i * k);
+            System.out.println(results);
+        }
+
+        catch (NumberFormatException e) {
+            System.out.println("Inputs n and k variables must be integers");
         }
     }
 }
