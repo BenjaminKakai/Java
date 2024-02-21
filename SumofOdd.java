ea9f0a44d06f4fbea621e6d0ac4d8b77503c4e42
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 11:21:31 +0300

    Add files via upload

---
+++ SumofOdd.java
@@ -0,0 +1,13 @@
+/*public class SumofOdd {
+
+    int sum = 0;
+    int index = 0;
+while (n > 0) {
+        if (index % 2 == 0) { // Sum digits at even indexes
+            sum += n % 10;
+        }
+        n /= 10;
+        index++;
+    }
+
+}*/
