6e904496b6c50cd0cab5c98d93ce9138f2492fa2
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 10:44:33 +0300

    Add files via upload

---
+++ HomogeneousArray.java
@@ -0,0 +1,9 @@
+public class HomogeneousArray {
+    public static void main (String[] args) {
+        String[] names = {"Benjamin", "Kakai", "Alpha", "Jayden"};
+
+        for (String i : names) {
+            System.out.println(i);
+        }
+    }
+}
