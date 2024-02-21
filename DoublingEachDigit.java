9676009e45b89976e13aca66970fa34b0e094c4d
benjamin kakai <benjaminkakai001@gmail.com>
2024-02-21 10:44:13 +0300

    Add files via upload

---
+++ DoublingEachDigit.java
@@ -0,0 +1,11 @@
+/*public class DoublingEachDigit {
+
+    int sum = 0;
+while (n > 0) {
+        int digit = (n % 10) * 2; // Double each digit
+        sum += digit;
+        n /= 10;
+    }
+
+}
+*/
